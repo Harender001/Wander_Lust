eeff7b654ad47d5165bf32b58f9c6db7607b32a7
Harender001 <harendersin82880@gmail.com>
2025-07-23 12:43:31 +0530

    Some error files added

--- init/index.js
+++ init/index.js
@@ -1,6 +1,6 @@
-const mongoose=require("mongoose");
-const initData=require("./data.js");
-const Listing=require("../models/listing.js");
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -17,11 +17,11 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-const initDB=async()=>{
-    //if initial data was there delete it
-    await Listing.deleteMany({});
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
+const initDB = async () => {
+  //if initial data was there delete it
+  await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
 };
 
 initDB();
--- models/listing.js
+++ models/listing.js
@@ -9,20 +9,29 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    filename:String,
+    filename: String,
     // type: String,
-    url:{
-      type:String,
-       //setting of default link in img
+    url: {
+      type: String,
+      //setting of default link in img
       default:
-      "https://images.unsplash.com/photo-1505852679233-d9fd70aff56d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1505852679233-d9fd70aff56d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       set: (v) =>
-      v === ""
-        ? "https://images.unsplash.com/photo-1505852679233-d9fd70aff56d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        : v, //ternary operator
+        v === ""
+          ? "https://images.unsplash.com/photo-1505852679233-d9fd70aff56d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          : v, //ternary operator
     },
-   
   },
+  // image: {
+  //     type: String,
+  //     default:
+  //       "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  //     set: (v) =>
+  //       v === ""
+  //         ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+  //         : v,
+  //   },
+
   price: Number,
   location: String,
   country: String,
